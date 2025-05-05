ad772c03f1fa21d855bbd9808ad24b899f3078d0
Alexandre Toulios <99737712+Lehzinho@users.noreply.github.com>
2025-05-05 21:02:11 +0000

    adiciona scripts `lint:fix` e `lint:check`

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,3 @@
-export default function Home () {
-    return (<div>Vivi e Elena eu te amo e sempre vou te amar!</div>)
+export default function Home() {
+  return <div>Vivi e Elena eu te amo e sempre vou te amar!</div>;
 }
