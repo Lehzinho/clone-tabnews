cd995b36836edbf9ae0de31273ab51fe02a7016f
Alexandre Toulios <99737712+Lehzinho@users.noreply.github.com>
2025-05-12 21:19:25 +0000

    finish ´/api/v1/status`endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,13 @@
 test("get to /api/v1/status sould return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
+
   expect(response.status).toBe(200);
+
+  const respoonseBody = await response.json();
+
+  const parseUpdatedAt = new Date(respoonseBody.updated_at).toISOString();
+  expect(respoonseBody.updated_at).toEqual(parseUpdatedAt);
+  expect(respoonseBody.dependencies.database.version).toEqual("16.0");
+  expect(respoonseBody.dependencies.database.max_connections).toEqual(100);
+  expect(respoonseBody.dependencies.database.opened_connections).toEqual(1);
 });
