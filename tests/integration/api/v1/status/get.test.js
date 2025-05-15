8b05e2c150783673ad4769b79db26bb64bc4493e
Alexandre Toulios <99737712+Lehzinho@users.noreply.github.com>
2025-05-15 16:15:04 +0000

    makes npm test more robust with `async-retry` and `orchestrator.js`

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,3 +1,9 @@
+import orchestrator from "tests/orchestrator";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+});
+
 test("get to /api/v1/status sould return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
 
